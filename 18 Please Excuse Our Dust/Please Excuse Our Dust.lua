f87ebbe6cdb811f417714b0516f8f4f2f8460656
CassandraLeo <aaronjfreed@gmail.com>
2018-05-09 19:07:26 -0400

    Fixed monster suffocation script for “Dust”/“Idol of Our Fear” (level 19 in selector, 18 in editors). Don’t have time right now to figure out why my attempt to condense it stopped working; I’ll look into it later

--- 18 Please Excuse Our Dust/Please Excuse Our Dust.lua
+++ 18 Please Excuse Our Dust/Please Excuse Our Dust.lua
@@ -27,12 +27,18 @@ function suffocate()
 	for p in Players() do		if p.z < suff2 and not p.dead then
 			p:damage(4,"suffocation")
 		elseif p.z < suff1 and not p.dead then			p:damage(1,"suffocation")		end
-	end	for m in Monsters() do		if m.z < suff2 and not m.player then			m.external_velocity = 0
-			if not m.dead then
+	end	for m in Monsters() do
+		if not m.player and not m.dead then
+			if m.z < suff2 then
 				m:damage(12,"suffocation")
+			elseif m.z < suff1 then
+				m:damage(4,"suffocation")
 			end
-		elseif m.z < suff1 and not m.dead and not m.player then			m:damage(4,"suffocation")
-		end	end
+		end
+		if m.z < suff2 and not m.player then
+		         m.external_velocity = 0
+		end
+	end
 end
 
 function darkness()
